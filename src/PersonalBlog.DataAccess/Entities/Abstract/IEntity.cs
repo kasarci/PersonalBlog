8510f8fa8324aeba2e326fbb8b5467b7e1bc268a
kasarci <muhammedkasarci@hotmail.com>
2022-10-31 15:22:14 +0100

    Added BsonId attribute to IEntity's Id property.

--- src/PersonalBlog.DataAccess/Entities/Abstract/IEntity.cs
+++ src/PersonalBlog.DataAccess/Entities/Abstract/IEntity.cs
@@ -1,7 +1,10 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace PersonalBlog.DataAccess.Entities.Abstract;
 
 public interface IEntity
 {
+    [BsonId]
     Guid Id { get; init; }
     DateTime CreatedAt { get; init; }
     DateTime? ModifiedAt { get; set; }
